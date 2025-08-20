7cea88965eeb703742393957b31d532980879273
akrgupta01 <akrgupta08@gmail.com>
2025-08-20 15:54:15 +0530

    changes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,4 @@
-// ✅ Correct for next.config.mjs
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://122.160.25.202/micron/app/api/api/:path*",
-      },
-    ]
-  },
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-export default nextConfig
+export default nextConfig;
