286236325ad00e17e35c84aa51e4c5af65762b70
akrgupta01 <akrgupta08@gmail.com>
2025-08-20 15:23:18 +0530

    ch

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// ✅ Correct for next.config.mjs
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://122.160.25.202/micron/app/api/api/:path*",
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
