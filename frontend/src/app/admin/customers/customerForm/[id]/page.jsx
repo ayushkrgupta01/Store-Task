3e0f0de4ffc1fbc18db43e7d2ccfc6b67faab043
akrgupta01 <akrgupta08@gmail.com>
2025-08-25 16:49:40 +0530

    Add customer title changes

--- frontend/src/app/admin/customers/customerForm/[id]/page.jsx
+++ frontend/src/app/admin/customers/customerForm/[id]/page.jsx
@@ -168,7 +168,7 @@ export default function CustomerFormForRandomStore() {
         {/* Header */}
         <div className="mb-8">
           <h1 className="text-2xl sm:text-3xl font-extrabold bg-gradient-to-r from-indigo-600 to-purple-600 bg-clip-text text-transparent">
-            Add Customer To Admin's Store
+            Add Customer
           </h1>
           <p className="text-gray-500 mt-1">
             Add New Customer with all required information
