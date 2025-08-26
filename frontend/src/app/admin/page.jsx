d4a3ae447c8350450bd4e787f2b9b06a65c9645e
akrgupta01 <akrgupta08@gmail.com>
2025-08-26 13:59:49 +0530

    removed dollar sign

--- frontend/src/app/admin/page.jsx
+++ frontend/src/app/admin/page.jsx
@@ -177,7 +177,7 @@ const AdminHome = () => {
                 </h2>
                 <p className="text-3xl font-bold text-gray-900">
                   {" "}
-                  ${totalSales.toLocaleString()}
+                  {totalSales.toLocaleString()}
                 </p>
               </div>
             </div>
