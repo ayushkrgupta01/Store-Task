01069e9cf747629a7c96441f4c5c21fb737a1e76
akrgupta01 <akrgupta08@gmail.com>
2025-08-27 18:00:51 +0530

    service icon changes

--- frontend/src/app/admin/allServices/page.jsx
+++ frontend/src/app/admin/allServices/page.jsx
@@ -472,7 +472,7 @@ const handleAddOrUpdateService = async (e) => {
                 onClick={handleOpenAddModal}
                 className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-lg flex items-center gap-2 transition-colors"
               >
-                <FaStore />
+                <FaProjectDiagram />
                 Add Service
               </button>
             </div>
